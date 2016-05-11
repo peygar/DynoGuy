ddc4aa177d060f0092b70a074da8fad57ff1d0bf
Peyman Halfmoon <Peyman@v1020-wn-67-155.campus-dynamic.uwaterloo.ca>
2016-05-11 17:31:46 -0400

    created play function

--- draft.cc
+++ draft.cc
@@ -1,8 +1,9 @@
 class Game
 {
-	Cell cells[10][60];
+	Cell cells[10][60]; 
+	Cacti *cacti;
+
 	Dinosaur *dino;
-	View *view;
 
 public:
 	Game();
@@ -16,24 +17,69 @@ class Cell {
 
 class View {
 	char cells[10][60];
-public:
 	void print();
 };
 
 class Dinosaur {
 	int distance;
 	int qMatrix[10][10];
+	int jumpProgress;
 public:
 	void jump();
 	bool shouldJump();
+	int getHeight();
+	void jumpIfNeed();
 };
+
+const int MAX_CACTUS_DISTANCE = 40;
+const int MIN_CACTUS_DISTANCE = 3;
+const int MAX_CACTUS_HEIGHT = 1;
+const int MIN_CACTUS_HEIGHT = 1;
+const int DINO_LOCATION = 5;
+typedef std::vector<pair<int,int> > Cacti;
 
 Game::Game() {
 	dino = new Dinosaur();
-	view = new View();
+	cacti = new Cacti;
 }
 
 Game::~Game() {
 	delete dino;
-	delete view;
+	delete cacti;
+}
+
+Game::createCactus() {
+	int height = rand() % (MAX_CACTUS_HEIGHT - MIN_CACTUS_HEIGHT + 1) + MIN_CACTUS_HEIGHT;
+	Cacti->insert(0, pair<int, int>(59, height));
+
+	for(int i = 0; i < height; i++)
+	{
+		cells[10 - i][59] = '#'; 
+	} 
+}
+
+//TODO: MOVE CACTUS
+
+Game::play() {
+	srand(time(NULL));
+
+	int distance = 0;
+	while(1) {
+		if (distance == 0)
+		{
+			distance = rand() % (MAX_CACTUS_DISTANCE - MIN_CACTUS_DISTANCE + 1) + MIN_CACTUS_DISTANCE;
+			createCactus();
+		}
+
+		if (kbhit()) {
+			dino->jump()
+		}
+
+		//move cacti
+		dinoHeight = dino->getHeight();
+
+		if (cacti->front.first == DINO_LOCATION && dinoHeight < cacti->front.second) {
+			EndGame(); 
+		}
+	}
 }
